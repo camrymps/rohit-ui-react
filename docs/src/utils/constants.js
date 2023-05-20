59f7aa28650d1b47900a780fa412d910edbce977
Michael Scott <camrymps@gmail.com>
2023-05-20 19:50:00 -0400

    Change semantic-ui-css package to fomantic-ui-css.

--- docs/src/utils/constants.js
+++ docs/src/utils/constants.js
@@ -1,5 +1,5 @@
 export const repoURL = 'https://github.com/Semantic-Org/Semantic-UI-React'
 export const typeOrder = ['element', 'collection', 'view', 'module', 'addon']
 export const semanticUIDocsURL = 'https://semantic-ui.com/'
-export const semanticUIRepoURL = 'https://github.com/Semantic-Org/Semantic-UI'
-export const semanticUICSSRepoURL = 'https://github.com/Semantic-Org/Semantic-UI-CSS'
+export const semanticUIRepoURL = 'https://github.com/fomantic/Fomantic-UI'
+export const semanticUICSSRepoURL = 'https://github.com/fomantic/Fomantic-UI-CSS'
